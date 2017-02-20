27b34a9469b6d98ac297e099f85ff6574f2b3bf0
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-02-20 05:03:50 -0800

    Added basic benchmark

---
+++ test/benchmark.cpp
@@ -0,0 +1,33 @@
+//  :copyright: (c) 2013-2017 Niels Lohmann <http://nlohmann.me>.
+//  :copyright: (c) 2015-2017 The Regents of the University of California.
+//  :license: MIT, see LICENSE.md for more details.
+/*
+ *  \addtogroup Tests
+ *  \brief Rudimentary benchmark for the JSON reader.
+ */
+
+#include <json/reader.hpp>
+#include <gtest/gtest.h>
+
+#include <chrono>
+
+
+// TESTS
+// -----
+
+
+TEST(Jeopardy, Benchmark)
+{
+    typedef std::chrono::high_resolution_clock Clock;
+
+    auto t1 = Clock::now();
+    json::FileTextReader reader("test/data/jeopardy.json");
+    do {
+        ASSERT_NO_THROW(reader.read());
+    } while (reader.isValid());
+    auto t2 = Clock::now();
+
+    std::cout << "Delta t2-t1: "
+              << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
+              << " nanoseconds" << std::endl;
+}
