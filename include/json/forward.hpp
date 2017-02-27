8ce7e7808667577279f2aad697dcd070292a4210
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-02-27 06:31:29 -0800

    Added forward definitions

---
+++ include/json/forward.hpp
@@ -0,0 +1,23 @@
+//  :copyright: (c) 2016-2017 The Regents of the University of California.
+//  :license: MIT, see LICENSE.md for more details.
+/**
+ *  \addtogroup JSON
+ *  \brief Forward definitions for JSON includes.
+ */
+
+#pragma once
+
+
+namespace json
+{
+// FORWARD
+// -------
+
+struct TextReader;
+struct FileTextReader;
+struct StringTextReader;
+struct TextWriter;
+struct FileTextWriter;
+struct StringTextWriter;
+
+}   /* json */
